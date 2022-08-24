49fddf3946f1bf275e523ca8a8c34f2bb18f60cb
psnbaotg <psnbaotg@protonmail.com>
2022-08-24 20:28:55 +0800

    refactor(components): rename some components for legibility

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import { ref } from 'vue';
 import SiteHeader from './components/SiteHeader.vue';
 import SiteFooter from './components/SiteFooter.vue';
-import DashboardButton from './components/DashboardButton.vue';
-import DashboardCard from './components/DashboardCard.vue';
+import WscdButton from './components/WscdButton.vue';
+import WscdCard from './components/WscdCard.vue';
 import ConnectivityChecker from './modules/ConnectivityChecker';
-import DashboardTable from './components/DashboardTable.vue';
+import DataTable from './components/DataTable.vue';
 import { DomainDataView, DomainCheckStatus, BlockStatus } from './types/view-model';
-import ProgressiveBar from './components/ProgressiveBar.vue';
+import WscdProgressiveBar from './components/WscdProgressiveBar.vue';
 
 type RawSiteList = Array<string | [string, boolean]>;
 
@@ -89,32 +89,32 @@ async function prefetchAndCheck(fetcher: () => Promise<{ default: unknown }>) {
     <SiteHeader></SiteHeader>
     <main>
       <div class="button-container">
-        <DashboardButton @click="prefetchAndCheck(() => import('./assets/sites-std.json'))"
+        <WscdButton @click="prefetchAndCheck(() => import('./assets/sites-std.json'))"
           :disabled="[CheckStatus.CHECKING, CheckStatus.PREFETCHING].includes(checkStatus)">
-          {{ $i18n('btn-std-chk') }}</DashboardButton>
-        <DashboardButton @click="prefetchAndCheck(() => import('./assets/sites-all.json'))"
+          {{ $i18n('btn-std-chk') }}</WscdButton>
+        <WscdButton @click="prefetchAndCheck(() => import('./assets/sites-all.json'))"
           :disabled="[CheckStatus.CHECKING, CheckStatus.PREFETCHING].includes(checkStatus)">
-          {{ $i18n('btn-ext-chk') }}</DashboardButton>
+          {{ $i18n('btn-ext-chk') }}</WscdButton>
       </div>
-      <ProgressiveBar class="bar-waiting" v-if="checkStatus === CheckStatus.PREFETCHING">
-      </ProgressiveBar>
-      <DashboardCard v-if="checkStatus === CheckStatus.ENDED_ERROR" type="error">
+      <WscdProgressiveBar class="bar-waiting" v-if="checkStatus === CheckStatus.PREFETCHING">
+      </WscdProgressiveBar>
+      <WscdCard v-if="checkStatus === CheckStatus.ENDED_ERROR" type="error">
         <template #header>{{ $i18n('card-err-head') }}</template>
         <template #default>
           {{ $i18n('card-err-desc') }}
         </template>
-      </DashboardCard>
-      <DashboardCard v-if="checkStatus === CheckStatus.NOT_CHECKED">
+      </WscdCard>
+      <WscdCard v-if="checkStatus === CheckStatus.NOT_CHECKED">
         <template #header>{{ $i18n('card-about-head') }}</template>
         <template #default>
           <!-- HTML is sanitized by banana-i18n -->
           <span v-html="$i18n('card-about-desc')">
           </span>
         </template>
-      </DashboardCard>
-      <DashboardTable :data="checkData"
+      </WscdCard>
+      <DataTable :data="checkData"
         v-if="checkStatus === CheckStatus.CHECKING || checkStatus == CheckStatus.ENDED">
-      </DashboardTable>
+      </DataTable>
     </main>
     <SiteFooter></SiteFooter>
   </div>
