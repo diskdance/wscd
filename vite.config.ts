f0896c6c6047fdfbe2a4754dadf1ba369fd1b1a1
psnbaotg <psnbaotg@protonmail.com>
2022-09-11 17:28:43 +0800

    build: minify html in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { execSync } from 'child_process';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
     GIT_HASH: JSON.stringify(execSync('git rev-parse --short HEAD').toString().trim()),
