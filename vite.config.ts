ce2bf6e2fc298c6e114dd23854d86d5fe0f83f3d
psnbaotg <psnbaotg@protonmail.com>
2022-10-05 20:42:41 +0800

    feat(pwa): add PWA support
    cool!

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { execSync } from 'child_process';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,31 @@ export default defineConfig({
       minify: true,
     }),
     splitVendorChunkPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Wikimedia Server Connectivity Dashboard',
+        short_name: 'wscd',
+        description: 'Tool telling your connectivity to Wikimedia servers',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
   ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
