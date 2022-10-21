0dd7227bf50121fa511f21650b62f587459a292d
psnbaotg <psnbaotg@protonmail.com>
2022-10-21 23:02:59 +0800

    refactor(DataTable): use composition API

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,94 +1,93 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import Banana from 'banana-i18n';
+import { computed, inject, ref } from 'vue';
 import store, { DomainBlockingResult, DomainConnectivityResult } from '../modules/store';
 import SiteButton from './SiteButton.vue';
 
 const STEPPING = 20;
 
-export default defineComponent({
-  data() {
-    return {
-      domainDataView: store.domainDataView,
-      start: 0,
-      STEPPING,
-    };
-  },
-  computed: {
-    domainDataViewSlice() {
-      return [...this.domainDataView.entries()].slice(this.start, this.start + STEPPING);
-    },
-    isAtStart() {
-      return this.start - STEPPING < 0;
-    },
-    isAtEnd() {
-      return this.start + STEPPING > this.domainDataView.size - 1;
-    },
-  },
-  methods: {
-    getName(domain: string): string {
-      const transformedDomain = domain.replace(/^(www|m)\./g, '').replace(/\.(www|m)\./g, '.');
-      const key = `name-${transformedDomain}`;
-      const translation = this.$i18n(key);
-      return translation === key ? this.$i18n('name-key-missing') : translation;
-    },
-    getStatus(status: DomainConnectivityResult): string {
-      switch (status) {
-        case DomainConnectivityResult.FAILURE:
-          return this.$i18n('avl-no');
-        case DomainConnectivityResult.SUCCESS:
-          return this.$i18n('avl-yes');
-        case DomainConnectivityResult.CHECKING:
-          return this.$i18n('checking');
-        default:
-          return this.$i18n('pending');
-      }
-    },
-    getDomainBlockingResult(blockStatus: DomainBlockingResult | undefined): string {
-      if (blockStatus === undefined) {
-        return '';
-      }
-      switch (blockStatus) {
-        case DomainBlockingResult.BLOCKED:
-          return this.$i18n('blk-yes');
-        case DomainBlockingResult.NOT_BLOCKED:
-          return this.$i18n('blk-no');
-        case DomainBlockingResult.NOT_A_WIKI:
-          return this.$i18n('blk-non-wiki');
-        default:
-          return this.$i18n('blk-unknown');
-      }
-    },
-    getConnectivity(ping: number | undefined): string {
-      if (ping === undefined) {
-        return '';
-      }
-      if (ping <= 1000) {
-        return this.$i18n('rtt-excellent');
-      }
-      if (ping > 1000 && ping <= 2000) {
-        return this.$i18n('rtt-good');
-      }
-      return this.$i18n('rtt-bad');
-    },
-    nextPage() {
-      if (!this.isAtEnd) {
-        this.start += STEPPING;
-      }
-    },
-    prevPage() {
-      if (!this.isAtStart) {
-        this.start -= STEPPING;
-      }
-    },
-    toStart() {
-      this.start = 0;
-    },
-    toEnd() {
-      this.start = this.domainDataView.size - (this.domainDataView.size % STEPPING);
-    },
-  },
-  components: { SiteButton },
-});
+const banana = inject<Banana>('banana')!;
+
+const start = ref(0);
+
+const domainDataViewSlice = computed(
+  () => [...store.domainDataView.entries()].slice(start.value, start.value + STEPPING),
+);
+
+const isAtStart = computed(() => start.value - STEPPING < 0);
+
+const isAtEnd = computed(
+  () => start.value + STEPPING > store.domainDataView.size - 1,
+);
+
+function getName(domain: string): string {
+  const transformedDomain = domain.replace(/^(www|m)\./g, '').replace(/\.(www|m)\./g, '.');
+  const key = `name-${transformedDomain}`;
+  const translation = banana.i18n(key);
+  return translation === key ? banana.i18n('name-key-missing') : translation;
+}
+
+function getStatus(status: DomainConnectivityResult): string {
+  switch (status) {
+    case DomainConnectivityResult.FAILURE:
+      return banana.i18n('avl-no');
+    case DomainConnectivityResult.SUCCESS:
+      return banana.i18n('avl-yes');
+    case DomainConnectivityResult.CHECKING:
+      return banana.i18n('checking');
+    default:
+      return banana.i18n('pending');
+  }
+}
+
+function getDomainBlockingResult(blockStatus: DomainBlockingResult | undefined): string {
+  if (blockStatus === undefined) {
+    return '';
+  }
+  switch (blockStatus) {
+    case DomainBlockingResult.BLOCKED:
+      return banana.i18n('blk-yes');
+    case DomainBlockingResult.NOT_BLOCKED:
+      return banana.i18n('blk-no');
+    case DomainBlockingResult.NOT_A_WIKI:
+      return banana.i18n('blk-non-wiki');
+    default:
+      return banana.i18n('blk-unknown');
+  }
+}
+
+function getConnectivity(ping: number | undefined): string {
+  if (ping === undefined) {
+    return '';
+  }
+  if (ping <= 1000) {
+    return banana.i18n('rtt-excellent');
+  }
+  if (ping > 1000 && ping <= 2000) {
+    return banana.i18n('rtt-good');
+  }
+  return banana.i18n('rtt-bad');
+}
+
+function nextPage() {
+  if (!isAtEnd.value) {
+    start.value += STEPPING;
+  }
+}
+
+function prevPage() {
+  if (!isAtStart.value) {
+    start.value -= STEPPING;
+  }
+}
+
+function toStart() {
+  start.value = 0;
+}
+
+function toEnd() {
+  start.value = store.domainDataView.size - (store.domainDataView.size % STEPPING);
+}
 </script>
 
 <template>
@@ -146,8 +145,8 @@ export default defineComponent({
         :class="['page__button', { 'pager__button--hidden': isAtStart }]"
         :title="$i18n('tbl-btn-prev')">&lt;
       </SiteButton>
-      <div>{{ $i18n('tbl-page-info', start + 1, isAtEnd ? domainDataView.size : start + STEPPING,
-      domainDataView.size) }}</div>
+      <div>{{ $i18n('tbl-page-info', start + 1, isAtEnd ? store.domainDataView.size : start +
+      STEPPING, store.domainDataView.size) }}</div>
       <SiteButton @click="nextPage()"
         :class="['page__button', { 'pager__button--hidden': isAtEnd }]"
         :title="$i18n('tbl-btn-next')">&gt;
