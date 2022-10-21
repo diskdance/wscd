825693ee5a8b15debdc5312a6eac25a2a9792058
psnbaotg <psnbaotg@protonmail.com>
2022-10-21 22:43:55 +0800

    feat(PWA): add dark mode theme color and maskable icons

--- vite.config.ts
+++ vite.config.ts
@@ -22,14 +22,16 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'maskable_icon_x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'maskable_icon_x512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
