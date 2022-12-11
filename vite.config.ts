6cac64c3e5406b163b221067e110f869982f37bc
psnbaotg <psnbaotg@protonmail.com>
2022-12-11 21:02:29 +0800

    build: set up autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { execSync } from 'child_process';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { VitePWA } from 'vite-plugin-pwa';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,13 @@ export default defineConfig({
       selfDestroying: true,
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}),
+      ],
+    },
+  },
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
     GIT_HASH: JSON.stringify(execSync('git rev-parse --short HEAD').toString().trim()),
